fedaba8cedc3a636fb06ad8194cc71967ca827a3
Ahmadfareedkhan <ahmadkhanfareed388@gmail.com>
2024-03-07 14:57:33 +0500

    count

--- Course2-programming_in_python/week2-assignment/lab1/ordering_system.py
+++ Course2-programming_in_python/week2-assignment/lab1/ordering_system.py
@@ -101,7 +101,7 @@ def take_order():
     count = 1
     for i in range(3):
         item = input('Select menu item number ' + str(count) + ' (from 1 to 5): ')
-        if item > 5:
+        if count > 5:
             print("Please Select Mention Number Only!")
         count += 1
         order.append(menu[int(item)])
