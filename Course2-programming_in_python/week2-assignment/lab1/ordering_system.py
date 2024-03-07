4710e1bac8074925ee6d7777544b2f16c7f5bdcd
Ahmadfareedkhan <ahmadkhanfareed388@gmail.com>
2024-03-07 15:32:48 +0500

    del

--- Course2-programming_in_python/week2-assignment/lab1/ordering_system.py
+++ Course2-programming_in_python/week2-assignment/lab1/ordering_system.py
@@ -101,9 +101,6 @@ def take_order():
     count = 1
     for i in range(3):
         item = input('Select menu item number ' + str(count) + ' (from 1 to 5): ')
-        for num in item:
-            if num > 5:
-                print("Invalid Number")
         count += 1
         order.append(menu[int(item)])
     return order
